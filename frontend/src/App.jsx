703f41960a71886e7044933a1963186f2db5557b
Raghuldev03 <raghulraghul1551@gmail.com>
2024-08-29 14:33:59 +0530

    Login Page UI

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,8 +4,6 @@ import "./App.css";
 function App() {
   return (
     <div className="container">
-      <h1 className="text-center">Hello, Bootstrap with Vite in React!</h1>
-      <button className="btn btn-primary">Click Me</button>
     </div>
   );
 }
