f18e032f35598ba4314d8620b2cb00bae7e75700
Raghuldev03 <raghulraghul1551@gmail.com>
2024-08-31 14:39:16 +0530

    get messaage functionality success

--- frontend/src/component/messsages/Message.jsx
+++ frontend/src/component/messsages/Message.jsx
@@ -1,14 +1,37 @@
 import React from "react";
 import userAvatar from "../../assets/user.png";
+import { useAuthContext } from "../../context/AuthContext";
+import useConversation from "../../../zustand/useConversation";
 
-const Message = () => {
+const Message = ({ message }) => {
+  console.log(message)
+  const { authUser } = useAuthContext();
+
+  const { selectedConversation } = useConversation();
+
+  const messageFromMe = message.senderId === authUser._id;
+  const chatClassName = messageFromMe ? "chat-end" : "chat-start";
+  
+  const profilePic = messageFromMe
+     ? authUser.profilepic
+     : selectedConversation?.profilepic;
+
+  const msgBgColor = messageFromMe ? "bg-success" : "";
+
   return (
-    <div className="d-flex justify-content-end mb-2">
-      <div className="bg-primary text-white rounded p-2">Hello</div>
+    <div className={`d-flex justify-content-end mb-2 chart ${chatClassName}`}>
+      <div className={`bg-primary text-white rounded p-2 ${msgBgColor}`}>
+        <p>{message.message}</p>
+        <div style={{ marginTop: -20}}>
+          <small style={{ fontSize: 10 }}>
+            {new Date(message.createdAt).toLocaleTimeString()}
+          </small>
+        </div>
+      </div>
       <div className="d-flex align-items-center">
         <div className="avatar me-2">
           <img
-            src={userAvatar}
+            src={profilePic}
             alt="User Avatar"
             className="rounded-circle"
             style={{ width: "40px", height: "40px" }}
--- frontend/src/component/messsages/Messages.jsx
+++ frontend/src/component/messsages/Messages.jsx
@@ -1,16 +1,31 @@
-import React from 'react'
-import Message from './Message'
+import React, { useRef } from "react";
+import Message from "./Message";
+import useGetMessages from "../../hooks/useGetMessages";
 
 const Messages = () => {
+  const { messages = [], loading } = useGetMessages();
+
+  // console.log("Messages:", messages); 
+  const lastMessageRef = useRef()
+  
+
   return (
-    <div>
-      <Message />
-      <Message />
-      <Message />
-      <Message />
-      <Message />
+    <div className="d-flex flex-column p-3 h-100 overflow-auto">
+      <div>
+        {/* <p>Loading: {loading.toString()}</p>
+        <p>Messages length: {messages.length}</p> */}
+        {!loading && messages.length === 0 && (
+          <p className="text-center text-secondary ">No messages yet.</p>
+        )}
+      </div>
+      {messages.length > 0 &&
+        messages.map((message) => (
+          <div key={message._id} className="mb-2">
+            <Message message={message} />
+          </div>
+        ))}
     </div>
   );
-}
+};
 
-export default Messages
+export default Messages;
